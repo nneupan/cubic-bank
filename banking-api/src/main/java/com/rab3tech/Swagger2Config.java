afc148da0ec8df51ea5dabcad4314ab5a09e52d5
nneupan <nilneu2007@gmail.com>
2023-05-13 17:41:42 -0400

    changes added

--- banking-api/src/main/java/com/rab3tech/Swagger2Config.java
+++ banking-api/src/main/java/com/rab3tech/Swagger2Config.java
@@ -21,6 +21,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Swagger2Config {
 	
+	
 	@Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
